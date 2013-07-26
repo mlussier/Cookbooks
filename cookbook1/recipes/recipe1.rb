7f15d7fbf491659f1e7e62ea58eeaf9c90cecf5c
Mark Lussier <mlussier@stempremier.com>
2013-07-26 10:22:38 -0400

    unzip stemp.zip

--- cookbook1/recipes/recipe1.rb
+++ cookbook1/recipes/recipe1.rb
@@ -1,15 +1,20 @@
 #
 # include_recipe "Chef::Log"
 Chef::Log.info "This is a console test to see if it works"
-directory "/home/ubuntu/STEMPremier" do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-end
+#directory "/home/ubuntu/STEMPremier" do
+#  owner "root"
+#  group "root"
+#  mode 00755
+#  action :create
+# end
 
 remote_file "/home/ubuntu/stem.zip" do
   source "https://s3.amazonaws.com/stempremier.com/stem.zip"
   mode 00755
 end
+
+execute "unzip" do
+  command "unzip -x /home/ubuntu/stemp.zip"
+end
+
 
