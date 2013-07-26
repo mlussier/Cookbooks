87a371bf5ab092083e09d3b2cc98dfa51079d5f3
Mark Lussier <mlussier@stempremier.com>
2013-07-26 09:43:01 -0400

    create Stem directory test

--- cookbook1/recipes/recipe1.rb
+++ cookbook1/recipes/recipe1.rb
@@ -1,3 +1,9 @@
 #
 # include_recipe "Chef::Log"
 Chef::Log.info "This is a console test to see if it works"
+directory "/home/ubuntu/STEMPremier" do
+  owner "root"
+  group "root"
+  mode 00644
+  action :create
+end
