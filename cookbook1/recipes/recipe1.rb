b7f301e36f510bddde0ef29a0be73b94db0dbee2
Mark Lussier <mlussier@stempremier.com>
2013-07-26 10:11:57 -0400

    copy zip file

--- cookbook1/recipes/recipe1.rb
+++ cookbook1/recipes/recipe1.rb
@@ -4,6 +4,12 @@ Chef::Log.info "This is a console test to see if it works"
 directory "/home/ubuntu/STEMPremier" do
   owner "root"
   group "root"
-  mode 00644
+  mode 00755
   action :create
 end
+
+remote_file "/home/ubuntu/stem.zip" do
+  source "https://s3.amazonaws.com/stempremier.com/stem.zip"
+  mode 00755
+end
+
